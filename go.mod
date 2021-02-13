eefb1cc1733a24cbc0f06784c393fd9f914463a4
Cloud User <centos@sameer-dev.novalocal>
2021-02-13 01:23:15 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/grafana/grafana-starter-datasource-backend
+
+go 1.15
+
+require (
+	github.com/grafana/grafana-plugin-sdk-go v0.86.0
+	github.com/vertica/vertica-sql-go v1.1.1
+)
---
+++ src/DataSource.ts
@@ -0,0 +1,71 @@
+import { DataSourceInstanceSettings, MetricFindValue, DataFrame, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { VerticaDataSourceOptions, VerticaQuery } from './types';
+
+export class DataSource extends DataSourceWithBackend<VerticaQuery, VerticaDataSourceOptions> {
+  templateSrv;
+  constructor(instanceSettings: DataSourceInstanceSettings<VerticaDataSourceOptions>) {
+    super(instanceSettings);
+    this.templateSrv = getTemplateSrv();
+  }
+
+  applyTemplateVariables(query: VerticaQuery, scopedVars: ScopedVars): VerticaQuery {
+    query.queryTemplated = this.templateSrv.replace(query.queryString, scopedVars);
+    return query;
+  }
+
+  async metricFindQuery(query: string, options?: any) {
+    const findVal: MetricFindValue[] = [];
+    if (!query) {
+      return Promise.resolve(findVal);
+    }
+    const response = await this.query({
+      targets: [
+        {
+          queryType: 'table',
+          queryString: query,
+          queryTemplated: query,
+        },
+      ],
+    } as any).toPromise();
+
+    if (response.error) {
+      throw new Error(response.error.message);
+    }
+
+    const data = response.data[0] as DataFrame;
+    const textField = data.fields.find((f) => f.name === '_text');
+    const valueField = data.fields.find((f) => f.name === '_value');
+
+    switch (true) {
+      case data.fields.length > 3:
+        throw new Error(
+          `Received more than two (${data.fields.length}) fields: ${data.fields.map((x) => x.name).join(',')}`
+        );
+        break;
+      case textField === undefined:
+        throw new Error(
+          `Variable query must have atleast one column with name '_text' fields: ${data.fields
+            .map((x) => x.name)
+            .join(',')}`
+        );
+        break;
+    }
+
+    for (let i = 0; i < data.fields[0].values.length; i++) {
+      let metricValue: MetricFindValue = {
+        text: '',
+        value: undefined,
+      } as MetricFindValue;
+      metricValue.text = textField?.values.toArray()[i];
+      if (valueField === undefined) {
+        metricValue.value = textField?.values.toArray()[i];
+      } else {
+        metricValue.value = valueField?.values.toArray()[i];
+      }
+
+      findVal.push(metricValue);
+    }
+    return findVal;
+  }
+}
